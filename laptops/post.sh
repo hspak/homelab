f8ec4fd74431d94a60f01b22d7b45b7b7f80b0fc
Hong Shick Pak <hong@hspak.com>
2024-04-20 21:24:54 -0700

    changes

--- laptops/post.sh
+++ laptops/post.sh
@@ -9,29 +9,29 @@
 sudo pacman -Syy
 
 sudo pacman -S \
-  base-devel go mpv ranger curl \a
-  gnome gdm python-neovim noto-fonts noto-fonts-emoji gnome-tweaks \
+  base-devel go mpv ranger curl \
+  plasma kde-applications sddm sddm-kcm neovim python-neovim noto-fonts noto-fonts-emoji \
   firefox firefox-i18n-ko firefox-i18n-en-us \
-  iw crda zsh usbutils man-db bluez-utils pavucontrol \
-  xdg-desktop-portal-wlr tailscale
+  iw crda zsh usbutils man-db bluez-utils \
+  tailscale wl-clipboard
 
 # Bootstrap AUR helper
-mkdir -p ~/tmp
-cd ~/tmp
+# mkdir -p ~/tmp
+# cd ~/tmp
 
 # TODO: changeme - I don't like how this is packaged
-curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/zig-master-bin.tar.gz
-tar xf zig-master-bin.tar.gz
-cd zig-master-bin
-makepkg -si
-
-cd ..
-curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/zur.tar.gz
-tar xf zur.tar.gz
-cd zur
-makepkg -si
-cd
-rm -rf ~/tmp
+# curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/zig-master-bin.tar.gz
+# tar xf zig-master-bin.tar.gz
+# cd zig-master-bin
+# makepkg -si
+# 
+# cd ..
+# curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/zur.tar.gz
+# tar xf zur.tar.gz
+# cd zur
+# makepkg -si
+# cd
+# rm -rf ~/tmp
 
 mkdir -p ~/code/z
 curl -o ~/code/z/z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
@@ -39,29 +39,15 @@ touch ~/.z
 
 # Just install all the fonts
 sudo pacman -S \
-  noto-font-extra ttf-fira-sans ttf-fira-mono ttf-ubuntu-font-family \
+  noto-font-extra ttf-fira-sans ttf-fira-mono \
   ttf-croscore ttf-roboto ttf-inconsolata ttf-opensans
-sudo zur -S ttf-merriweather ttf-merriweather-sans ttf-oswald ttf-quintessential ttf-signika
-
-# Terminal
-sudo pacman -S libutf8proc check ttf-dejavu
-zur -S foot-terminfo fcft tllist
-zur -S foot-git
-
-# Sway stuff
-pacamn -S swayidle swaybg wlroots swaylock sway
-zur -S grim-git mako-git wl-clipboard-git \
-  ttf-google-fonts-git ttf-font-awesome-4 \
-  brightnessztl i3status-rust-git 
+# sudo zur -S ttf-merriweather ttf-merriweather-sans ttf-oswald ttf-quintessential ttf-signika
 
 git clone https://github.com/hspak/dotfiles.git
 cd dotfiles
 ./setup
 cd
 
-# Arch neovim treesitter is stale
-zur -S neovim-git
-
 # passwords
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | gpg --import
-zur -S 1password
+# zur -S 1password
